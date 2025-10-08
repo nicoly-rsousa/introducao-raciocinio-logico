7b59a9183b94d9ad0ba1b40a4f344be1d2d65fb6
Nicoly <nicolyrrh@gmail.com>
2025-10-08 11:34:18 -0300

    exercicios-resolvidos

--- exercicios/respostas-exe-introdutorios.js
+++ exercicios/respostas-exe-introdutorios.js
@@ -1,16 +1,15 @@
 //1. Se ontem foi quarta-feira, amanhã será...
 //R: sexta
-console.log('Se ontem foi quarta amanhã será')
+console.log('1. Se ontem foi quarta amanhã será')
 console.log('HOJE : QUINTA')
 console.log('AMANHÃ : SEXTA')
 console.log("------------------------------------------------------------------------------")
+
 console.log('2. Complete a sequência: 1, 4, 9, 16, 25, ...')
-//R: 36
 console.log('1, 4, 9, 16, 25')
 console.log('1+3, 4+5, 9+7, 16+9, 25+11')
 console.log('=36')
 console.log("------------------------------------------------------------------------------")
-
 
 console.log('3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?')
 console.log('R: 50 KM == 50000 metros')
@@ -19,79 +18,110 @@ console.log('(150 * 60)/50000=0,18m ')
 console.log('0,18*60= 10,8 segundos')
 console.log("------------------------------------------------------------------------------")
 
-//4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?
+console.log("4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?")
 console.log("R:Júlia ou Bruno")
 console.log("------------------------------------------------------------------------------")
-//5. Em uma sala, há 2 pais e 2 filhos. Qual o número mínimo de pessoas na sala?
+
+console.log('5. Em uma sala, há 2 pais e 2 filhos. Qual o número mínimo de pessoas na sala?')
 console.log("R:3 (pai, avô e filho")
-
 console.log("------------------------------------------------------------------------------")
-//6. Se o código para "GATO" é 4130, qual é o código para "PATO"?
+
+console.log('6. Se o código para "GATO" é 4130, qual é o código para "PATO"?')
 console.log('R: 9130')
-
 console.log("------------------------------------------------------------------------------")
-//7. Você tem 50 moedas de 1 real e 50 moedas de 50 centavos. No total, quanto você tem?
+
+console.log('7. Você tem 50 moedas de 1 real e 50 moedas de 50 centavos. No total, quanto você tem?')
 console.log('R: 75')
-
 console.log("------------------------------------------------------------------------------")
-//8. Uma torneira pinga 1 gota a cada segundo. Quantas gotas ela pingará em 1 minuto?
+
+console.log('8. Uma torneira pinga 1 gota a cada segundo. Quantas gotas ela pingará em 1 minuto?')
 console.log('R: 60 gotas')
 console.log("------------------------------------------------------------------------------")
-//9. Se todos os corvos são pretos e minha caneta é um corvo, então minha caneta é preta. Essa afirmação é logicamente válida?
+
+console.log('9. Se todos os corvos são pretos e minha caneta é um corvo, então minha caneta é preta. Essa afirmação é logicamente válida?')
 console.log('R:Sim, é uma afirmação válida.')
 console.log("------------------------------------------------------------------------------")
 
-//10. Se 3 coelhos comem 3 cenouras em 3 minutos, quanto tempo 10 coelhos levarão para comer 10 cenouras?
+console.log('10. Se 3 coelhos comem 3 cenouras em 3 minutos, quanto tempo 10 coelhos levarão para comer 10 cenouras?')
 console.log('R: 3 minutos              3 - 3  - 3')
 console.log('                         10 - 10 - 10 ') 
 console.log("------------------------------------------------------------------------------")
 
-//11. Se "SOL" está para "LUZ" como "GELO" está para...
+console.log('11. Se "SOL" está para "LUZ" como "GELO" está para...')
 console.log(' R: Frio')
-
 console.log("------------------------------------------------------------------------------")
-//12. João, Pedro e Maria têm 3 profissões diferentes: médico, professor e engenheiro. João não é médico. Pedro é amigo do engenheiro. Quem é o professor?
+
+console.log('12. João, Pedro e Maria têm 3 profissões diferentes: médico, professor e engenheiro. João não é médico. Pedro é amigo do engenheiro. Quem é o professor?')
 console.log(' R:João')
-
 console.log("------------------------------------------------------------------------------")
-//13. Uma caixa de bombons custa 10 reais. Se a caixa custa 8 reais a mais que os bombons, qual o preço dos bombons?
+
+console.log('13. Uma caixa de bombons custa 10 reais. Se a caixa custa 8 reais a mais que os bombons, qual o preço dos bombons?')
 console.log(' R: 2x+8=10')
-//x=1
-
+console.log('x=1')
 console.log("------------------------------------------------------------------------------")
-//14. Um relógio digital mostra 08:08. Qual será a próxima vez que ele mostrará a mesma configuração de dígitos?
-//R:
 
+console.log('14. Um relógio digital mostra 08:08. Qual será a próxima vez que ele mostrará a mesma configuração de dígitos?')
+console.log('R:Depois de 24 horas')
+console.log("------------------------------------------------------------------------------")
 
-//15. Se o dobro de um número é 20, qual é a metade do triplo desse número?
-//R:
-//16. Em um grupo de 100 pessoas, 85 falam inglês e 75 falam espanhol. Quantas pessoas, no mínimo, falam as duas línguas?
-//R:
-//17. Se "A" está para 1, "B" para 2, "C" para 3, etc., qual é a soma de "C" + "O" + "D" + "I" + "G" + "O"?
-//R:
-//18. Em um torneio, cada equipe joga uma vez contra todas as outras. Se há 5 equipes, quantos jogos serão disputados no total?
-//R:
-//19. Um homem foi ao restaurante, comeu o que quis e pagou com uma nota de 50 reais, mas o garçom lhe devolveu 20 reais. Quanto ele gastou?
-//R:
-//20. Se 50% de um número é 40, quanto é 25% desse mesmo número?
-//R:
-//21. Uma corrida de revezamento tem 4 corredores. O primeiro corre 10 metros, o segundo corre 20, o terceiro 40, e o quarto 80. Qual é a distância total da corrida?
-//R:
-//22. Se a palavra "AMOR" está para "ROMA" como "CASA" está para...
-//R:
-//23. Em um grupo, 3 pessoas mentem e 2 falam a verdade. Quem diz: "Pelo menos um de nós está mentindo"?
-//R:
-//24. Se a temperatura sobe de 10°C para 20°C, quantos graus ela subiu?
-//R:
-//25. Se um carro viaja a 60 km/h, quantos quilômetros ele percorre em 30 minutos?
-//R:
-//26. Se a irmã de Clara tem um irmão, e esse irmão tem 3 irmãos, quantas irmãs Clara tem?
-//R:
-//27. Qual o próximo número na sequência: 1, 1, 2, 3, 5, 8, ...
-//R:
-//28. Se um pato anda, um peixe nada e um pássaro voa, então um canguru...
-//R:
-//29. Se todos os cães latem e Bob é um cão, então Bob late. Essa afirmação é válida?
-//R:
-//30. Um lago tem um crescimento de plantas que dobra de área a cada dia. Se em 30 dias o lago fica coberto, em quantos dias ele estava coberto pela metade?
-//R:
+console.log("15. Se o dobro de um número é 20, qual é a metade do triplo desse número?")
+console.log('R: 15')
+console.log("------------------------------------------------------------------------------")
+
+console.log('16. Em um grupo de 100 pessoas, 85 falam inglês e 75 falam espanhol. Quantas pessoas, no mínimo, falam as duas línguas?')
+console.log('R:Falam ingles / espanhol 160 --- > 100-160=60 pessoas falam as duas línguas')
+console.log("------------------------------------------------------------------------------")
+
+console.log('17. Se "A" está para 1, "B" para 2, "C" para 3, etc., qual é a soma de "C" + "O" + "D" + "I" + "G" + "O"')
+console.log('R: 3+15+4+9+7+15=53')
+console.log("------------------------------------------------------------------------------")
+
+console.log('18. Em um torneio, cada equipe joga uma vez contra todas as outras. Se há 5 equipes, quantos jogos serão disputados no total?')
+console.log('R:total de jogos 10')
+console.log("------------------------------------------------------------------------------")
+
+console.log('19. Um homem foi ao restaurante, comeu o que quis e pagou com uma nota de 50 reais, mas o garçom lhe devolveu 20 reais. Quanto ele gastou?')
+console.log('R:30 reais')
+console.log('------------------------------------------------------------------------------')
+
+console.log('20. Se 50% de um número é 40, quanto é 25% desse mesmo número?')
+console.log('R: 20')
+console.log("------------------------------------------------------------------------------")
+
+console.log('21. Uma corrida de revezamento tem 4 corredores. O primeiro corre 10 metros, o segundo corre 20, o terceiro 40, e o quarto 80. Qual é a distância total da corrida?')
+console.log('R: 150 metros')
+console.log("------------------------------------------------------------------------------")
+console.log('22. Se a palavra "AMOR" está para "ROMA" como "CASA" está para...')
+console.log('R: ASCA')
+console.log("------------------------------------------------------------------------------")
+
+console.log('23. Em um grupo, 3 pessoas mentem e 2 falam a verdade. Quem diz: "Pelo menos um de nós está mentindo"?')
+console.log('R: Quem fala a verdade')
+console.log("------------------------------------------------------------------------------")
+console.log('24. Se a temperatura sobe de 10°C para 20°C, quantos graus ela subiu?')
+console.log('R: 10 graus')
+console.log("------------------------------------------------------------------------------")
+
+console.log('25. Se um carro viaja a 60 km/h, quantos quilômetros ele percorre em 30 minutos?')
+console.log('R: 30 km')
+console.log("------------------------------------------------------------------------------")
+
+console.log('26. Se a irmã de Clara tem um irmão, e esse irmão tem 3 irmãos, quantas irmãs Clara tem?')
+console.log('R: 1 irmã')
+console.log("------------------------------------------------------------------------------")
+
+console.log('27. Qual o próximo número na sequência: 1, 1, 2, 3, 5, 8, ...')
+console.log('R: 13')
+console.log("------------------------------------------------------------------------------")
+
+console.log('28. Se um pato anda, um peixe nada e um pássaro voa, então um canguru...')
+console.log('R: pula')
+console.log("------------------------------------------------------------------------------")
+
+console.log('29. Se todos os cães latem e Bob é um cão, então Bob late. Essa afirmação é válida?')
+console.log('R: Sim, é uma afirmação válida.')
+console.log("------------------------------------------------------------------------------")
+
+console.log('30. Um lago tem um crescimento de plantas que dobra de área a cada dia. Se em 30 dias o lago fica coberto, em quantos dias ele estava coberto pela metade?')
+console.log('R: 29 dias, porque dobra a cada dia')
+console.log("------------------------------------------------------------------------------")
